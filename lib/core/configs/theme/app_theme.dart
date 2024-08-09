87540d17e57edb4e27819b87bdf6c94a07508038
Syed-Farazuddin <syedfarazuddin73@gmail.com>
2024-08-09 06:21:31 +0530

    folder structure done

---
+++ lib/core/configs/theme/app_theme.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/core/configs/theme/theme.dart';
+
+class AppTheme {
+  // Light theme
+
+  static final lightTheme = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.light,
+    fontFamily: "Satoshi",
+    brightness: Brightness.light,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+    ),
+  );
+
+  // Dark Theme
+
+  static final darkThemes = ThemeData(
+    primaryColor: AppColors.primary,
+    scaffoldBackgroundColor: AppColors.dark,
+    fontFamily: "Satoshi",
+    brightness: Brightness.dark,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.primary,
+        textStyle: const TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+      ),
+    ),
+  );
+}
